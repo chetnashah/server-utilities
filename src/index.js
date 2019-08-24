6066cb5fa4c464d6a5ea2ec0af40e44e4aa56609
chetnashah <chetshah80@gmail.com>
2019-08-24 22:43:50 +0530

    adfadf

--- src/index.js
+++ src/index.js
@@ -17,7 +17,18 @@ import {
 } from 'passport-local';
 import bcrypt from 'bcryptjs';
 
-app.use(morgan('combined'))
+app.use(
+    morgan(function (tokens, req, res) {
+        return [
+          tokens.method(req, res),
+          tokens.url(req, res),
+          tokens.status(req, res),
+          tokens.res(req, res, 'content-length'), '-',
+          tokens['response-time'](req, res),
+          'msssssssssss'
+        ].join(' ')
+      })      
+)
 
 var admin = require('firebase-admin');
 var serviceAccount = require("../envfiles/service-utilities-firebase-adminsdk-bhw83-0b99ce539e.json");
